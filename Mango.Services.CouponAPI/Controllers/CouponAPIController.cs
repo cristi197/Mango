3156a61bc08e82a3aaa761424570eab842bc01eb
Cristi <cristian.teodorescu97@yahoo.ro>
2023-07-08 15:57:23 +0300

    Add a common response and add Coupon Controller

---
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,55 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        private ResponseDto _responseDto;
+
+        public CouponAPIController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+            _responseDto = new ResponseDto();
+        }
+
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> coupons = _appDbContext.Coupons.ToList();
+                _responseDto.Result = coupons;
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto; 
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto GetById(int id)
+        {
+            try
+            {
+                Coupon coupons = _appDbContext.Coupons.First(u=> u.CouponId == id);
+                _responseDto.Result = coupons;
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+    }
+}
