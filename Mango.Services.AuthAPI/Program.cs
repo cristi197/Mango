80dffcf0be1f8d9c1f2b3ee4eca228f05ba1bb08
Cristi <cristian.teodorescu97@yahoo.ro>
2023-07-19 11:37:55 +0300

    Configure JwtOptions

--- Mango.Services.AuthAPI/Program.cs
+++ Mango.Services.AuthAPI/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddDbContext<AppDbContext>(option =>
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
 
+builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("ApiSettings:JwtOptions"));
+
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
