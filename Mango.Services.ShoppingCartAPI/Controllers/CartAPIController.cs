b3c5dc8969754b43355edea80a50966a23723798
Cristi <cristian.teodorescu97@yahoo.ro>
2023-07-25 12:46:18 +0300

    Add cart upsert logic

---
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Data;
+using Mango.Services.ShoppingCartAPI.Models;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ShoppingCartAPI.Controllers
+{
+    [Route("api/cart")]
+    [ApiController]
+    public class CartAPIController : ControllerBase
+    {
+        private ResponseDto _response;
+        private IMapper _mapper;
+        private readonly AppDbContext _appDbContext;
+
+        public CartAPIController(AppDbContext appDbContext, IMapper mapper)
+        {
+            _appDbContext = appDbContext;
+            _mapper = mapper;
+            _response = new ResponseDto();
+        }
+
+        [HttpPost("CartUpsert")]
+        public async Task<ResponseDto> CartUpsert(CartDto cartDto)
+        {
+            try
+            {
+                var cartHeaderFromDb = await _appDbContext.CartHeaders.FirstOrDefaultAsync(u => u.UserId == cartDto.CartHeader.UserId);
+
+                if (cartHeaderFromDb == null)
+                {
+                    //create cart header and details
+                    CartHeader cartHeader = _mapper.Map<CartHeader>(cartDto.CartHeader);
+                    _appDbContext.CartHeaders.Add(cartHeader);
+                    await _appDbContext.SaveChangesAsync();
+
+                    cartDto.CartDetails.First().CartHeaderId = cartHeader.CartHeaderId;
+
+                    CartDetails cartDetails = _mapper.Map<CartDetails>(cartDto.CartDetails.First());
+
+                    _appDbContext.CartDetails.Add(cartDetails);
+                    await _appDbContext.SaveChangesAsync();
+                }
+                else
+                {
+                    //if header is not null, check if details has same product
+
+                    var cartDetailsFromDb = await _appDbContext.CartDetails.FirstOrDefaultAsync(
+                                                u => u.ProductId == cartDto.CartDetails.First().ProductId && 
+                                                u.CartHeaderId == cartHeaderFromDb.CartHeaderId);
+
+                    if(cartDetailsFromDb == null)
+                    {
+                        //create cartDetails
+                    }
+                    else
+                    {
+                        //update count in cart details
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.Message.ToString();
+                _response.IsSuccess = false;
+            }
+        }
+    }
+}
