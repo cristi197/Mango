b20ab32a344ef612974d5ecde76fdbd88f76a8b3
Cristi <cristian.teodorescu97@yahoo.ro>
2023-07-28 14:09:17 +0300

    Create EmailAPI project and add DTO's and models

--- Mango.Services.EmailAPI/Models/Dto/CartHeaderDto.cs
+++ Mango.Services.EmailAPI/Models/Dto/CartHeaderDto.cs
@@ -1,7 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-
-namespace Mango.Services.ShoppingCartAPI.Models.Dto
+﻿namespace Mango.Services.EmailAPI.Models.Dto
 {
     public class CartHeaderDto
     {
