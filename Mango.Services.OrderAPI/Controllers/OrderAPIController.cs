28c5c26ba9975024a5453c02f368f2a0abac27d8
Cristi <cristian.teodorescu97@yahoo.ro>
2023-08-18 11:07:40 +0300

    Add GetAllOrders and GetSpecificOrder endpoints

--- Mango.Services.OrderAPI/Controllers/OrderAPIController.cs
+++ Mango.Services.OrderAPI/Controllers/OrderAPIController.cs
@@ -8,6 +8,7 @@ using Mango.Services.ShoppingCartAPI.Service.IService;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Stripe;
 using Stripe.Checkout;
 
@@ -33,6 +34,52 @@ namespace Mango.Services.OrderAPI.Controllers
             _mapper = mapper;
             _messageBus = messageBus;
             _configuration = configuration;
+        }
+
+        [Authorize]
+        [HttpGet("GetOrders")]
+        public ResponseDto? Get(string? userId = "")
+        {
+            try
+            {
+                IEnumerable<OrderHeader> orderHeaders;
+
+                if (User.IsInRole(SD.RoleAdmin))
+                {
+                    orderHeaders = _db.OrderHeaders.Include(u => u.OrderDetails).OrderByDescending(u => u.OrderHeaderId).ToList();
+                }
+                else
+                {
+                    orderHeaders = _db.OrderHeaders.Include(u => u.OrderDetails).Where(u => u.UserId == userId).OrderByDescending(u => u.OrderHeaderId).ToList();
+                }
+
+                _response.Result = _mapper.Map<IEnumerable<OrderHeaderDto>>(orderHeaders);  
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.Message;
+                _response.IsSuccess = false;
+            }
+
+            return _response;
+        }
+
+        [Authorize]
+        [HttpGet("GetOrder/{id:int}")]
+        public ResponseDto? Get(int id)
+        {
+            try
+            {
+                OrderHeader orderHeader = _db.OrderHeaders.Include(u => u.OrderDetails).First(u => u.OrderHeaderId == id);
+                _response.Result = _mapper.Map<OrderHeaderDto>(orderHeader);
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.Message;
+                _response.IsSuccess = false;
+            }
+
+            return _response;
         }
 
         [Authorize]
@@ -101,7 +148,7 @@ namespace Mango.Services.OrderAPI.Controllers
                     options.LineItems.Add(sessionLineItem);
                 }
 
-                if(stripeRequestDto.OrderHeader.Discount > 0)
+                if (stripeRequestDto.OrderHeader.Discount > 0)
                 {
                     options.Discounts = discounts;
                 }
@@ -138,7 +185,7 @@ namespace Mango.Services.OrderAPI.Controllers
                 var paymentIntentService = new PaymentIntentService();
                 PaymentIntent paymentIntent = paymentIntentService.Get(session.PaymentIntentId);
 
-                if(paymentIntent.Status == "succeeded")
+                if (paymentIntent.Status == "succeeded")
                 {
                     //payment was successful
                     orderHeader.PaymentIntentId = paymentIntent.Id;
@@ -155,7 +202,7 @@ namespace Mango.Services.OrderAPI.Controllers
                     string topicName = _configuration.GetValue<string>("TopicAndQueueNames:OrderCreatedTopic");
                     await _messageBus.PublishMessage(rewardsDto, topicName);
 
-                    _response.Result = _mapper.Map<OrderHeaderDto>(orderHeader);   
+                    _response.Result = _mapper.Map<OrderHeaderDto>(orderHeader);
                 }
             }
             catch (Exception ex)
