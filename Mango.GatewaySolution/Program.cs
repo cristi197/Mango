6f252b4357c461d7f9ed48e7e5ca79b51635a4c1
Cristi <cristian.teodorescu97@yahoo.ro>
2023-08-20 11:22:32 +0300

    Make some clean in appsetting file, split ocelot configuration in a separate file

--- Mango.GatewaySolution/Program.cs
+++ Mango.GatewaySolution/Program.cs
@@ -3,11 +3,11 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddOcelot();
+builder.Configuration.AddJsonFile("ocelot.json",optional:false, reloadOnChange:true);
+builder.Services.AddOcelot(builder.Configuration);
 builder.AddAppAuthentication();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.UseOcelot();
+app.UseOcelot().GetAwaiter().GetResult();
 app.Run();
